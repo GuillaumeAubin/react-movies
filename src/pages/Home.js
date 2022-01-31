5dc8cb16e5a5d909fbff87cc2d1e56e6428b5ced
Guillaume Aubin <aubinguillaume4@gmail.com>
2022-01-31 19:55:12 +0100

    Genre Finder Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import Form from "../components/Form";
+import Header from "../components/Header";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>HOME</h1>
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <Header />
+      <Form />
+    </div>
+  );
 };
 
 export default Home;
